674bdf64caa42794dba358afc6b02b5022afff8e
Rishabh Chaudhari <73410445+Raporaz03@users.noreply.github.com>
2023-05-24 11:08:49 +0530

    userRouter completed

--- methods.js
+++ methods.js
@@ -7,43 +7,81 @@ app.listen(3000);
 
 let users =[
 {
-    "id":1,
-    'name':'raja'
+    id:1,
+    name:'raja'
 
 },
 {
-    "id":2,
-    'name':'rishabh'
+    id:2,
+    name:'rishabh'
 
 },
 {
-    "id":3,
-    'name':'prangel'
+    id:3,
+    name:'prangel'
 
 },
 {
-    "id":4,
-    'name':'snek'
+    id:4,
+    name:'snek'
 
 }
 ];
 
-app.get('/user',(req,res)=>{
-    console.log(req.query);
-    res.send(users);
-})
+// /Creating mini app
 
-app.post('/user',(req,res)=>{
+const userRouter=express.Router();
+const authRouter=express.Router();
+// BASE ROUTER
+app.use("/user",userRouter);
+app.use("/auth",authRouter);
+
+userRouter
+.route('/')
+.get(getUser)
+.post(postUser)
+.patch(updateUser)
+.delete(deleteUser)
+
+userRouter
+.route('/:id')
+.get(getUserById)
+
+
+// app.get('/user',(req,res)=>{
+//     console.log(req.query);
+//     res.send(users);
+// })
+
+// /mounting
+
+// app.get('/user',)
+// app.post('/user',)
+// app.patch('/user',)
+// app.delete('/user',)
+
+//params
+// app.get('/user/:username',)
+
+
+//mouting
+
+function getUser(req,res){
+        console.log(req.query);
+        res.send(users);
+        
+    }
+
+function postUser(req,res){
     console.log(req.body)
     users=req.body
     res.json({
         message :"data received successfully",
         user:req.body
     })
-})
+}    
 
-//update->patch
-app.patch('/user',(req,res)=>{
+function updateUser(req,res){
     console.log('req.body->',req.body  );
 
     //update data in user object
@@ -54,20 +92,26 @@ app.patch('/user',(req,res)=>{
     res.json(
 {        message:"data updated successfully"
 }    )
-})
+}
 
-//to delete a data
-app.delete('/user',(req,res)=>{
+function deleteUser(req,res){
     users={}
     res.json({
         message:"data has been deleted"
     })
-})
+}
 
-//params
-app.get('/user/:username',(req,res)=>{
-    console.log(req.params.username);
-    console.log(req.params);
-    res.send("user id is recieved");
-
-})
+function getUserById(req,res){
+    console.log(req.params.id);
+    let paramId=req.params.id;
+    let obj={};
+    for(let i=0;i<users.length;i++){
+        if(users[i]['id']==paramId){
+            obj=users[i];
+        }
+    }
+    res.json({
+        message:"req receiced",
+        data:obj
+    });
+}
